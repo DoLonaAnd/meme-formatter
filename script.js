04c1f82ca9fbc9d08463701bd7b39159cf44859a
Lona <103243217+DoLonaAnd@users.noreply.github.com>
2022-12-28 08:58:39 +0900

    頑張った。

--- script.js
+++ script.js
@@ -8,6 +8,12 @@
  *  [for innerHTML
  *      [elements id],
  *      [words]
+ *  ],
+ *  [for style
+ *      [elements id],
+ *      [size],
+ *      [style]
+ *  0 = font-size
  *  ]
  */
 const words_list = [
@@ -30,13 +36,28 @@ const words_list = [
         ]
     ],
     [
-        ["buttonSplitting"],
+        ["buttonSplitting", "usageH", "usage"],
         [
             [
                 "Splitting",
                 "分割する"
+            ],
+            [
+                "Usage",
+                "使い方"
+            ],
+            [
+                "Enter the title you want to enter in the input box \"Title you want to split\".<br>Enter the characters to use for splitting in the input box \"Characters for splitting\".<br>When you press the \"Splitting\" button, <br>the result will be displayed in the output box \"Splitted title\".",
+                "入力ボックス『分割したいタイトル』に分割したいタイトルを入力します。<br>入力ボックス『分割に使う文字』を入力します。<br>『分割する』ボタンを押すと、出力ボックス『分割したタイトル』に<br>分割したタイトルが表示されます。"
             ]
         ]
+    ],
+    [
+        ["usage"],
+        [
+            ["133.33%", "100%"]
+        ],
+        [0]
     ]
 ]
 const langs = [
@@ -66,6 +87,15 @@ function setHtml(i, num) {
         case 1:
             for (let i = 0; i < lists[0].length; i++) {
                 document.getElementById(lists[0][i]).innerHTML = lists[1][i][num];
+            }
+            break;
+        case 2:
+            for (let i = 0; i < lists[0].length; i++) {
+                switch (lists[2]) {
+                    case 0:
+                        document.getElementById(lists[0][i]).style.fontSize = lists[1][i][num];
+                        break;
+                }
             }
             break;
         default:
